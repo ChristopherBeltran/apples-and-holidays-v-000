4f4136004ee2ca914d209900e940d9e040f20062
Christopher Beltran <christod.beltran@gmail.com>
2018-07-14 05:57:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash each do |season, holiday_name|
+  holiday_hash.each do |season, holiday_name|
     holiday_name.each do |supplies|
       if supplies == "BBQ"
         return holiday_name
