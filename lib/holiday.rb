ea1028204c1824a90883ec786066b8f1036eadbd
Christopher Beltran <christod.beltran@gmail.com>
2018-07-14 05:52:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,10 +46,9 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize! + ":"
-    puts holiday.to_s.capitalize! + ":"
-    holiday.each do |supplies|
-      puts supplies 
+    puts "#{season.to_s.capitalize!}:"
+    holiday.each do |holiday_name, supplies|
+      puts "  #{holiday_name.to_s.split("_").collect {|split_holiday_name| split_holiday_name.capitalize}.join(" ")}: #{supplies.join(", ")}" 
     end
   end
   
