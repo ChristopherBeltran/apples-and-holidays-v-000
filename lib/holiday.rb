74f49e39db5446e8ada3a06b9b2f96fdfaaf785a
Christopher Beltran <christod.beltran@gmail.com>
2018-07-14 04:21:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
-    puts holiday
+    puts holiday.to_s.capitalize + ":"
     holiday.each do |supplies|
       puts supplies 
     end
